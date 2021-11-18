2a65645c3169128f1f1b18b8812d8e3d128f27c0
Sami Wasta <samiwasta.11@gmail.com>
2021-11-19 00:45:41 +0530

    Add files via upload

---
+++ Quiz_Website/js/questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Address of a website is known as _______",
+        answer: "Uniform Resource Locator",
+        options: [
+            "Universe Resource Locator",
+            "Uniform Resource Location",
+            "Uniform Resource Locator",
+            "Universe Resource Location"
+        ]
+    },
+    {
+        numb: 2,
+        question: "What is the full form of OCR?",
+        answer: "Optical Character Recognition",
+        options: [
+            "Optical Character Recognition",
+            "Optical Central Reader",
+            "Optimized Character Reader",
+            "Optical Character Response"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What is the use of <strong> in HTML?",
+        answer: "Makes the text as important",
+        options: [
+            "Displays as added text",
+            "Makes the text as important",
+            "Highlights the text",
+            "Displays as strike out text"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Machine Learning is the subset of?",
+        answer: "Artificial Intelligence",
+        options: [
+            "Deep Learning",
+            "Data Learing",
+            "Artificial Intelligence",
+            "None of the above"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which is vaild C expression?",
+        answer: "int my_num = 100000",
+        options: [
+            "int my_num = 100,000",
+            "int my_num = 100000",
+            "int my num = 100000",
+            "int $my_num = 100000"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Which is the equality operator?",
+        answer: "==",
+        options: [
+            ">=",
+            "<=",
+            "!=",
+            "=="
+        ]
+    },
+    {
+        numb: 7,
+        question: "What does CSS stands for?",
+        answer: "Cascading Style Sheet",
+        options: [
+            "Common Style Sheet",
+            "Colorful Style Sheet",
+            "Computer Style Sheet",
+            "Cascading Style Sheet"
+        ]
+    },
+    {
+        numb: 8,
+        question: "What does PHP stands for?",
+        answer: "Hypertext Preprocessor",
+        options: [
+            "Hypertext Preprocessor",
+            "Hypertext Programming",
+            "Hypertext Preprogramming",
+            "Hometext Preprocessor"
+        ]
+    },
+    {
+        numb: 9,
+        question: "What does SQL stands for?",
+        answer: "Structured Query Language",
+        options: [
+            "Stylish Question Language",
+            "Stylesheet Query Language",
+            "Statement Question Language",
+            "Structured Query Language"
+        ]
+    },
+    {
+        numb: 10,
+        question: "What does XML stands for?",
+        answer: "EXtensible Markup Language",
+        options: [
+            "EXtra Modern Link",
+            "EXtensible Markup Language",
+            "Example Markup Language",
+            "X-Markup Language"
+        ]
+    },
+]
